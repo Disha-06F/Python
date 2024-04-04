6dfc41b80391ccd5fd5f7ae8d50b631129ababa1
Hasna Hena Joti <disha.amin025@gmail.com>
2024-04-04 06:59:43 +0600

    type() built in function

---
+++ type_built_in_function.py
@@ -0,0 +1,8 @@
+Name="Hi I am ___"
+Age=21
+current_income=30.001
+#now all the above mentioned variable data types 
+#can be found with the help of "type()" built in function
+print(type(Name)) # 'str'
+print(type(Age)) # 'int'
+print(type(current_income))# 'float'
